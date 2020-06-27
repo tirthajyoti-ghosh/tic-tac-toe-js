74c004a1475bcd2bf628313aec8a3d5a8fd3b0b0
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-06-27 15:51:31 +0530

    add isWinner logic to game module

--- script/logic.js
+++ script/logic.js
@@ -1,10 +1,21 @@
 /* eslint-disable no-unused-vars */
 
 const game = (() => {
-  const winConmbinatio = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]];
+  const winCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]];
 
-  
+  const isWinner = (moves) => {
+    for (const comb of winCombinations) {
+      const intersection = moves.filter(value => comb.includes(value)).sort();
+
+      if (JSON.stringify(intersection) === JSON.stringify(comb)) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+
+  return {
+    isWinner
+  };
 })();
-const result = () => function (cells) {
-
-};
